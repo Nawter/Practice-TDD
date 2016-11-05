6b0a44128e59afc5dd48b9897ecf966c563d71f3
zaiddawoodissa <zaiddawoodissa@gmail.com>
2016-11-05 12:58:41 +0000

    Sale decides to display the price.

--- test/source/ca/jetbrains/pos/test/SellOneItemTest.java
+++ test/source/ca/jetbrains/pos/test/SellOneItemTest.java
@@ -1,5 +1,6 @@
 package ca.jetbrains.pos.test;
 
+import org.junit.Ignore;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
@@ -11,19 +12,40 @@ public class SellOneItemTest {
     @Test
     public void productFound() throws Exception {
         final Display display=new Display();
-        final Sale sale= new Sale();
+        final Sale sale= new Sale(display);
         sale.onBarcode("12345");
         assertEquals("$7.95",display.getText());
     }
 
+    @Test
+    @Ignore("Refactoring..........")
+    public void anotherProductFound() throws Exception {
+        final Display display=new Display();
+        final Sale sale= new Sale(display);
+        sale.onBarcode("54321");
+        assertEquals("$12.50",display.getText());
+    }
+
     public static class Display {
+
+        private String text;
+
         public String getText() {
-            return "$7.95";
+            return text;
+        }
+
+        public void setText(String text) {
+            this.text = text;
         }
     }
 
     public static class Sale {
+        private Display display;
+        public Sale(Display display) {
+            this.display = display;
+        }
         public void onBarcode(String barcode) {
+            display.setText("$7.95");
         }
     }
 }
